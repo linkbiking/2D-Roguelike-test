084a65a5f9ca1d75ea287831989efe124fda44e7
linkbiking <linkbiking@gmail.com>
2017-06-07 01:15:31 +0700

    Hello git hub world
    test first time use git hub

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour 
+{
+
+	public GameObject gameManager;
+	public GameObject soundManager;
+	// Use this for initialization
+	void Awake () 
+	{
+		if (GameManager.instance == null)
+			Instantiate (gameManager);
+
+		//if (SoundManager == null) 
+		//	Instantiate(soundManager);
+	
+
+	}
+}
